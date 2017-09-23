7516b42675e09fdfc7a44f7e8beb57b4e97c8430
unknown <davidaleksix@gmial.com>
2017-09-23 18:37:46 +0200

    Week2 homework

---
+++ wp-content/themes/Assigment1/index.php
@@ -0,0 +1,22 @@
+<?php get_header() ?>
+<nav id="main-menu">
+    <ul>
+        <li><a href="#">Page</a></li>
+        <li><a href="#">Page</a></li>
+        <li><a href="#">Page</a></li>
+        <li><a href="#">Page</a></li>
+    </ul>
+</nav>
+
+<main>
+    <h1><?php echo get_bloginfo('name');?>
+        ~
+<?php echo get_bloginfo('description'); ?>
+        </h1>
+    <div class="content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo nemo ea quibusdam quo quasi sunt neque modi hic quisquam necessitatibus in expedita officia a iure, nihil dolor harum amet eius.</p>
+        <p>Exercitationem perspiciatis unde, incidunt id delectus, quidem maiores quia, laudantium maxime repellat nobis itaque ipsum dolores officiis. Eos ut accusamus fugit voluptas facere deserunt, ipsam ducimus et delectus, eligendi odit.</p>
+    </div>
+</main>
+
+<?php get_footer()?>
